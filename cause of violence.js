c69653865608c24b1aaf685616efc60e7b02810e
brando2010 <bugromero72@gmail.com>
2024-08-12 14:52:35 -0400

    test

---
+++ cause of violence.js
@@ -0,0 +1,31 @@
+const ptag = document.getElementsByClassName('group1')
+console.log(ptag)
+function Green(){
+    for (var i = 0; i < ptag.length; i++) {
+        ptag[i].style.color = 'green'
+    }
+}
+function Navy(){
+
+    for (var i = 0; i < ptag.length; i++) {
+        ptag[i].style.color = 'navy'
+    }
+}
+
+function Aqua(){
+    for (var i = 0; i < ptag.length; i++) {
+        ptag[i].style.color = 'Aqua'
+    }
+}
+
+function Purple(){
+    for (var i = 0; i < ptag.length; i++) {
+        ptag[i].style.color = 'Purple'
+    }
+}
+
+function Gold(){
+    for (var i = 0; i < ptag.length; i++) {
+        ptag[i].style.color = 'Gold'
+    }
+}
